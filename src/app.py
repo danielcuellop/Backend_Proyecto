fba6a07cbeed1c41ce5e98280fc5532cbb5330b1
danielcuellop <121581478+danielcuellop@users.noreply.github.com>
2023-06-15 00:30:25 +0000

    Push 14/06

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Muestra
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,77 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+# Metodo para hacer el GET de los Usuarios
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users = User.query.all()
+    result = []
+    for user in users:
+        result.append({
+            "id": user.id,
+            "name": user.name,
+            "last_name": user.last_name,
+            "rut": user.rut,
+            "email": user.email,
+            "rol": user.rol
+        })
+    return jsonify(result)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+
+    user = User(
+        name=data['name'],
+        last_name=data['last_name'],
+        rut=data['rut'],
+        email=data['email'],
+        rol=data['rol'],
+        password=data['password']
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'message': 'Usuario creado correctamente'})
+
+
+@app.route('/muestra', methods=['POST'])
+def create_muestra():
+    data = request.json
+
+    muestra = Muestra(
+        project_name=data['project_name'],
+        ubication=data['ubication'],
+        ubication_image=data['ubication_image'],
+        area=data['area'],
+        specimen=data['specimen'],
+        quality_specimen=data['quality_specimen'],
+        image_specimen=data['image_specimen'],
+        aditional_comments=data['aditional_comments']
+    )
+
+    db.session.add(muestra)
+    db.session.commit()
+
+    return jsonify({'message': 'Muestra  creada correctamente'})
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
